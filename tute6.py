87582f4cc030a05f35203c67520bc9f9bce946d6
araiz45 <araizzahid45@gmail.com>
2024-02-11 01:34:21 +0500

    completed 6

---
+++ tute6.py
@@ -0,0 +1,18 @@
+# String 
+name = "Araiz"
+friendName = "Iqrar"
+# for multiline strings 
+after = ''' I'm araiz I'm a good boy 
+cause I don't know how to make a good deceicion in mylife
+'''
+print(after)
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+# print (name[5]) Error Index Error
+
+# Looping through an String
+for char in after:
+    print(char)
