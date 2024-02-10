2d9cf59083f015e9015f896262276e61353f9321
araiz45 <araizzahid45@gmail.com>
2024-02-11 00:03:08 +0500

    compelted 5

--- tute2.py
+++ tute2.py
@@ -12,3 +12,10 @@ this is multiline comments
 more than you think
 
 """
+
+print('Hey I\'m a good boy')
+
+print ('Hey', 5, 7, sep="~",end="\n003") 
+# end specify what to print at the end
+# sep specify how to separate the object 
+print("hellow world")
