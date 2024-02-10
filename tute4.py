bb06f22895433139bdf3dcb886522b3053228ec6
araiz45 <araizzahid45@gmail.com>
2024-02-11 00:57:22 +0500

    TypeCasting

---
+++ tute4.py
@@ -0,0 +1,30 @@
+# TypeCasting
+
+a = "1"
+b = "2"
+a = 1
+b = 2
+print(a + b)
+# Methods for typeCasting
+# str()
+# int()
+# float()
+# ord()
+# hex()
+# oct()
+# tuple()
+# set()
+# list()
+# dict()
+
+# name = int("a") - This gives error
+# print(name)
+
+# Two types of typeCasting
+# 1. Explicit - On my will
+# 2. Implicit - On Python will
+
+# Example of Implicit typeCasting
+c = 1.4
+d = 3
+print (c + d)
