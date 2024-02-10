b68d0a199d1dde9a4363371a4ced93eefd5c425b
araiz45 <araizzahid45@gmail.com>
2024-02-11 00:37:50 +0500

    calculator excercise

---
+++ ex1.py
@@ -0,0 +1,19 @@
+# Excercise 1 : Calculator
+num1 = int(input("Enter Your First Number: "))
+num2 = int(input ("Enter Your Second Number: "))
+oper = input("Enter Operator: ")
+
+if(oper == "+"):
+    print("The sum of num1 and num2 is: ", num1 + num2)
+elif (oper == "-"):
+    print("The sub of num1 and num2 is: ", num1 - num2)
+elif (oper == "*"):
+    print("The multi of num1 and num2 is: ", num1 * num2)
+elif (oper == "/"):
+    print("The divide of num1 and num2 is: ", num1 / num2)
+elif (oper == "%"):
+    print("The modulus of num1 and num2 is: ", num1 % num2)
+elif (oper == "//"):
+    print("The floar division of num1 and num2 is: ", num1 // num2)
+else:
+    print("Nothing is here")
