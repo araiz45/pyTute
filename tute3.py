1c74110475ed3289dd19afab2fd3e35871e3ea09
araiz45 <araizzahid45@gmail.com>
2024-02-11 00:24:39 +0500

    3 completed

---
+++ tute3.py
@@ -0,0 +1,28 @@
+# DataTypes and Variables
+a = 1
+print(1)
+
+b = "araiz"
+print(b)
+
+c = True
+print(c)
+
+d = False
+print (type(d))
+
+e = complex(5, 32)
+print(e)
+print(type(e))
+
+# Types of DataTypes in Python 
+
+# 1. int
+# 2. float
+# 3. complex
+# 4. bool
+# Sequence Data
+# 1. list
+# 2. tuple
+# Mapped Data
+# 1. dict 
