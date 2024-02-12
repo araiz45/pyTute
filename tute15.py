166498102266426e083acf8699e343ea38cb4866
araiz45 <araizzahid45@gmail.com>
2024-02-13 01:27:36 +0500

    tute 15 completed

---
+++ tute15.py
@@ -0,0 +1,18 @@
+i = 0
+
+# while (i <= 4):
+#     print("hellow orld", i)
+#     i = i + 1
+# print('done with the loop')
+
+# while (i <= 33):
+#     i = int(input("Enter Your Value: "))
+#     print("the number is ", i * 3.142)
+
+j = -5
+while (j > 0):
+    print(j)
+    j = j - 1
+
+else:
+    print("I'm using else with while")
