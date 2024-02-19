14b10bb62a5dce4b3ade91bca39a453bb0c52d62
araiz45 <araizzahid45@gmail.com>
2024-02-19 22:25:40 +0500

    tute25 completed

---
+++ tute25.py
@@ -0,0 +1,12 @@
+# Doc-String and PEP - 8 
+# Doc-String
+def square (n):
+    '''Take a break so you can rest and make yourself comfortable for this is instance of time'''
+    print(n**2)
+
+
+square(4)
+# to get declaration value 
+print(square.__doc__)
+
+# PEP - 8
