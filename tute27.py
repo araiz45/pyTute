fc70d17c9d3e77b168d622f99508ff952aa4c86b
araiz45 <araizzahid45@gmail.com>
2024-02-19 23:22:16 +0500

    tute27.py completed

---
+++ tute27.py
@@ -0,0 +1,12 @@
+# introduction to sets
+# Set is a collection of well-defined object
+sets = {3,5, 2, 4,2 ,3 }
+print(sets)
+sets2 = {3, "Araiz", True, "HUman", 3, 5}
+print(sets2)
+# Empty set
+set3 = set()
+print(type(set3))
+# Accessing
+for value in sets2:
+    print(value)
