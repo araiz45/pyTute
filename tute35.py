6d82e93e2f24da7829aa086ed850fcd1a72b73d1
araiz45 <araizzahid45@gmail.com>
2024-02-20 21:16:13 +0500

    tute35 completed

---
+++ tute35.py
@@ -0,0 +1,7 @@
+# Short hand If - else statement
+a = int(input("Enter the value: "))
+print("A") if a == 10 else print("=") if a == 44 else print("B")
+a = 33
+b = 36
+c = 9 if a > b else 0
+print(c)
