7884b430c7ac532f2c836e99a2b7af488c182c24
araiz45 <araizzahid45@gmail.com>
2024-02-21 23:49:59 +0500

    tute half

---
+++ tute37.py
@@ -0,0 +1,11 @@
+import math
+from math import sqrt, pi
+# from math import *       | Not Recommended
+import math as araiz
+from math import cos as c
+num = int(input("Enter Value which you want to make square: "))
+print(int(math.pow(num, 2)))
+print(int(sqrt(num)))
+print(num * pi)
+print(araiz.pi * 5)
+print(c(num))
---
+++ tute38.py
@@ -0,0 +1,5 @@
+import datetime as time 
+import os
+print(dir(os))
+# print(dir(time))
+print(os.cpu_count())
