08266f7e343da0f35eb762c945b547b79e3dd6c1
araiz45 <araizzahid45@gmail.com>
2024-03-02 13:00:31 +0500

    tute64.py

--- tute64.py
+++ tute64.py
@@ -1,12 +1,14 @@
 # Instance Variables vs Class Variables
 class Employee:
     companyName = "InternetCo"
+    noOfEmployee = 0
     def __init__(self, name):
         self.name = name
         self.pansion = 11
+        Employee.noOfEmployee += 1
 
     def ShowDetails(self):
-        print(f"The name of the Employee is {self.name} of company {self.companyName} and pasion is {self.pansion}")
+        print(f"The name of the Employee is {self.name} of company {self.companyName} with staff of {self.noOfEmployee} and pasion is {self.pansion}")
 
 
 araiz = Employee("araiz")
