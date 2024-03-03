f689672a5e5332e4fbaffdb563d7e8e19540de40
araiz45 <araizzahid45@gmail.com>
2024-03-04 01:45:15 +0500

    tute85.py

---
+++ tute84.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+# Copy file 
+# shutil.copy('tute84.py', "tute842.py")
+# Copy folder 
+# shutil.copytree("pdf", "araiz pdf")
+# move file
+# shutil.move("pdf/3.pdf", "araiz.pdf")
+# Remove a folder
+# shutil.rmtree("araiz pdf")
+# Remove a file 
+os.remove("araiz.pdf")
