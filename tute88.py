a260700b01bc0f7ef8c15e7b5b72471cb9ba12ee
araiz45 <araizzahid45@gmail.com>
2024-03-04 18:59:14 +0500

    tute88.py

---
+++ tute88.py
@@ -0,0 +1,19 @@
+# Generators
+
+def myGenerator():
+    for i in range(10):
+        yield i # Instead of Return
+
+
+gen = myGenerator()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+for j in gen:
+    print(j)
+    # pass
+
